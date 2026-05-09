fcb1c09ccba3b5df596914c185e530e19ad2f732
Aileeeeee <hannahonuigbo13@gmail.com>
2026-05-09 09:50:41 +0100

    updated project settings

--- backend/safepulse_backend/settings.py
+++ backend/safepulse_backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1jzde0vjv7^f)9jd$2#-y3s^b4kiax-q$z_*kay#7wh!y1!jgs"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,9 +39,21 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    # Third party apps
+    "incidents",
+    "accounts",
+
+    # Third_party packages
+    "rest_framework",
+    "restframework_simplejwt"
+    "drf_spectacular",
+    "corsheaders"
+
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -115,3 +129,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASS" : ['rest_framework.authentication.TokenAuthentication'],
+    "DEFAULT_SCHEMA_CLASS" : ['drf_spectacular.openai.AutoSchema'],
+    "DEFAULT_PAGINATION_CLASS" : 'rest_framework.pagination.PageNumberPagination',
+    "PAGE_SIZE" : 30
+}
+
+
+SPECTACULAR_SETTINGS = {
+    "TITLE" : "SafePulse API",
+    "DESCRIPTION" : "API for managing incident reports and Pulse alerts.",
+    "VERSION" : "1.0.0",
+    "SERVE_INCLUDE_SCHEMA" : False
+}
