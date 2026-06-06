a6938785caf290b4cd289fcad3a0da7358d2e2ea
Aileeeeee <hannahonuigbo13@gmail.com>
2026-06-06 11:59:18 +0100

    update changes

--- backend/sms/urls.py
+++ backend/sms/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('receive/',        views.SMSReceiveView.as_view()),
     path('ussd/stealth/',   views.StealthPulseView.as_view()),
+    path('ussd/', views.USSDView.as_view(), name='ussd'),
 ]
